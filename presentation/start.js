2b34913672b9e18429651440b1e9e04198201adc
David MAURIN <d.maurin@groupeonepoint.com>
2024-05-19 18:02:14 +0200

    add presentation tools

---
+++ presentation/start.js
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import asciidoctor from '@asciidoctor/core';
+import asciidoctorRevealjs from '@asciidoctor/reveal.js';
+import browserSync from 'browser-sync';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const converter = asciidoctor();
+const bs = browserSync.create();
+asciidoctorRevealjs.register();
+
+const convertAsciidoc = () => {
+  converter.convertFile('index.adoc', {
+    safe: 'safe',
+    backend: 'revealjs',
+    to_dir: 'dist',
+    mkdirs: true,
+    attributes: {
+      revealjsdir: `/dist/reveal.js`
+    }
+  });
+  fs.cpSync('../node_modules/reveal.js', 'dist/reveal.js', {recursive: true});
+  fs.cpSync('./slides/images', 'dist/images', {recursive: true});
+  fs.copyFileSync('css/custom.css', 'dist/custom.css');
+  fs.copyFileSync('../node_modules/highlightjs/styles/atom-one-dark.css', 'dist/atom-one-dark.css');
+}
+fs.rmSync(path.resolve(__dirname, 'dist'), { recursive: true, force: true })
+convertAsciidoc();
+
+bs.watch(['**/*.adoc', 'css/**.css']).on('change', () => {
+  convertAsciidoc();
+  bs.reload();
+});
+
+bs.init({
+  server: true,
+  startPath: 'dist'
+});
