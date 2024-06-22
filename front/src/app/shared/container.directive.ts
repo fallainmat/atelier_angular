5960d7bcd003c943d14e26b2974b698092ae3459
David MAURIN <d.maurin@groupeonepoint.com>
2024-06-22 16:38:38 +0200

    changes theme

--- front/src/app/shared/container.directive.ts
+++ front/src/app/shared/container.directive.ts
@@ -5,8 +5,9 @@ import { Directive, HostBinding, input } from '@angular/core';
   standalone: true,
   host: {
     '[class.bg-sky-900]': 'style() === "sky"',
-    '[class.bg-slate-200]': 'style() === "light"',
-    '[class.shadow-inner-xl]': 'style() === "light"',
+    '[class.bg-slate-100]': 'style() === "light"',
+    '[class.shadow-lg]': 'style() === "light"',
+    '[class.shadow-slate-900/40]': 'style() === "light"',
     '[class.m-2]': 'margin() === true',
     '[class.mb-0]': 'margin() === true'
   }
