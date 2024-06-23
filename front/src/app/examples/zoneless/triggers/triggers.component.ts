78f321e49f4b9323879f6764913539a294d5dd32
David MAURIN <d.maurin@groupeonepoint.com>
2024-06-23 19:11:02 +0200

    adds zoneless async demo

--- front/src/app/examples/zoneless/triggers/triggers.component.ts
+++ front/src/app/examples/zoneless/triggers/triggers.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { interval, map, pairwise, startWith } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { ContainerDirective } from '../../../shared/container.directive';
 
 @Component({
   selector: 'app-triggers.component.ts',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    ContainerDirective
+  ],
   templateUrl: './triggers.component.html',
   styleUrl: './triggers.component.scss'
 })
 export class TriggersComponent {
+  @ViewChild('vcr', { read: ViewContainerRef }) vcr!: ViewContainerRef;
 
+  @ViewChild('dynTemplate', { read: TemplateRef }) dynTemplate!: TemplateRef<unknown>;
+
+  timerObs$ = interval(1000).pipe(
+    takeUntilDestroyed(),
+    startWith(0),
+    pairwise(),
+    map(([prev, curr]) => prev + 1)
+  );
+
+  addToView() {
+    const template = this.dynTemplate;
+    const vcr = this.vcr;
+    if (vcr && template) {
+      vcr.createEmbeddedView(template)
+    }
+  }
+
+  clearView() {
+    this.vcr.clear();
+  }
 }
